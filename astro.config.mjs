3185c0dcd08fb3b2ecef70799efb8a2fe8099e0e
Luis Vasquez <luisvbdr0301@gmail.com>
2025-06-03 15:39:05 -0400

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -17,4 +19,6 @@ export default defineConfig({
     ,
     react(),
   ],
+
+  adapter: netlify(),
 });
