b56ec7157e194e5cd985965a6f022c936488e294
Luis Vasquez <luisvbdr0301@gmail.com>
2025-07-15 18:02:21 -0400

    delete google env var

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  prefetch: true,
   integrations: [
     sanity({
       projectId: "08bh5kcw",
